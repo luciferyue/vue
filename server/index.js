5ff9bd774f1ac294a198e546cee0efb637e82371
Lucifer <591662725@qq.com>
2020-07-20 23:25:23 +0800

    vue permission

--- server/index.js
+++ server/index.js
@@ -4,33 +4,35 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+	bodyParser.urlencoded({
+		extended: true
+	})
 );
 
 app.post("/user/login", (req, res) => {
-  const { username } = req.body;
+	const { username } = req.body;
 
-  if (username === "admin" || username === "jerry") {
-    res.json({
-      code: 1,
-      data: username
-    });
-  } else {
-    res.json({
-      code: 10204,
-      message: "用户名或密码错误"
-    });
-  }
+	if (username === "admin" || username === "jerry") {
+		res.json({
+			code: 1,
+			data: username
+		});
+	} else {
+		res.json({
+			code: 10204,
+			message: "用户名或密码错误"
+		});
+	}
 });
 
 app.get("/user/info", (req, res) => {
-  const roles = req.headers['authorization'].split(' ')[1] ? ["admin"] : ["editor"];
-  res.json({
-    code: 1,
-    data: roles
-  });
+	const roles = req.headers['authorization'].split(' ')[1] ? ["admin"] : ["editor"];
+	res.json({
+		code: 1,
+		data: roles
+	});
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+	console.log("chenggong");
+});
