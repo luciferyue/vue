671ec8f9020e3f0e9d77c75c9f5f2a4dc0adea7a
岳修斌 <xiubin.yue@guanaitong.com>
2020-08-07 23:40:47 +0800

    antd-pro

--- src/pages/more/index.js
+++ src/pages/more/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { Form, Input, Button, Card, Table } from 'antd';
-import { connect } from 'umi';
-import styles from './index.less';
+import styles from './index.css';
+import ProTable from '@ant-design/pro-table';
+import { getChannelData } from './service';
 
 const columns = [
   {
@@ -21,71 +20,22 @@ const columns = [
     key: 'city',
   },
 ];
+export default () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Page more/index</h1>
 
-// UI层和数据层分开
-class More extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {
-    this.props.getProductData({ name: '' });
-  }
-
-  // 成功才会执行这个函数
-  onFinish = values => {
-    console.log('values', values); // sy-log
-    // this.props.getMoreDataBySearch(values);
-    this.props.getProductData(values);
-  };
-
-  // 失败才会执行这个函数
-  onFinishFailed = err => {
-    console.log('err', err); // sy-log
-  };
-
-  render() {
-    const { data } = this.props.more;
-    return (
-      <PageHeaderWrapper className={styles.more}>
-        <Card>
-          <Form onFinish={this.onFinish} onFinishFailed={this.onFinishFailed}>
-            <Form.Item
-              label="姓名"
-              name="name"
-              rules={[{ required: true, message: '请输入姓名' }]}
-            >
-              <Input placeholder="请输入姓名" />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                查询
-              </Button>
-            </Form.Item>
-          </Form>
-        </Card>
-
-        <Card>
-          <Table columns={columns} dataSource={data} rowKey="id" />
-        </Card>
-      </PageHeaderWrapper>
-    );
-  }
-}
-
-export default connect(
-  // mapStateToProps
-  ({ more }) => ({ more }),
-  // mapDispatchToProps
-  {
-    getProductData: values => ({
-      type: 'more/getProductData',
-      payload: values,
-    }),
-    // getMoreDataBySearch: values => ({
-    //   type: 'more/getMoreDataBySearch',
-    //   payload: values,
-    // }),
-  },
-)(More);
+      <ProTable
+        rowKey="id"
+        columns={columns}
+        pagination={{
+          current: 1,
+          pageSize: 10,
+        }}
+        request={params => {
+          return getChannelData(params);
+        }}
+      ></ProTable>
+    </div>
+  );
+};
