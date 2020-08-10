106e62f2cdffd61f95ab6a2fb197a91b2395a195
Lucifer <591662725@qq.com>
2020-08-10 22:51:00 +0800

    my react

--- src/services/table.js
+++
@@ -1,9 +0,0 @@
-import request from '../utils/request';
-
-export async function getTablelData(params) {
-  console.log(params);
-  return request('/api/getTableData', {
-    data: params,
-    method: 'post',
-  });
-}
