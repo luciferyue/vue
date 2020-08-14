b68b911ed54b8c322c09cf5290b573cc4fd61908
Lucifer <591662725@qq.com>
2020-08-14 23:29:33 +0800

    hooks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 // import ContextPage from "./pages/ContextPage";
 // import AntdFormPage from "./pages/AntdFormPage";
 // import MyRCFieldForm from "./pages/MyRCFieldForm";
-import MyForm from './pages/MyForm'
+// import MyForm from './pages/MyForm'
+import UseCallbackPage from "./pages/UseCallbackPage"
+import UseMemoPage from "./pages/UseMemoPage"
 
 export default function App(props) {
 	return (
 		<>
-			<MyForm />
+			<UseCallbackPage />
+			<UseMemoPage />
 		</>
 	);
 }
