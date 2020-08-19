567e6723bdbc460a411ff425931e5fe4d70cafd5
岳修斌 <xiubin.yue@guanaitong.com>
2020-08-19 23:10:27 +0800

    第一节课作业

--- ex01/index.js
+++ ex01/index.js
@@ -1,6 +1,22 @@
-const { resolve } = require('path')
-const fs = require('fs')
+const { resolve } = require('path');
+const fs = require('fs');
+// 暗号：递归
 module.exports.getRouter = (path = resolve('./')) => {
-    // ##END##
+    // const list = fs.readFileSync(path);
+    const list = fs.readdirSync(path)
+    return `
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+${list.map(file =>
+        `{
+    path: '/${file.replace('.vue', '')}',
+    name: '${file.replace('.vue', '')}',
+    component: () => import('./views/${file}')
+},
+`).join("")}
+    ]
+})`
 }
 
