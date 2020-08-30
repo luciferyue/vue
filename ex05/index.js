a140546eb94b0deb7720aca8ef693d391b2c55e4
岳修斌 <xiubin.yue@guanaitong.com>
2020-08-30 15:41:37 +0800

    05

--- ex05/index.js
+++ ex05/index.js
@@ -1,16 +1,28 @@
-const {EventEmitter} = require('events')
+const { EventEmitter } = require('events')
 module.exports = class Connection {
-    // ##BEGIN## 代码已加密
-JEHJEHJEHJEHOSJOEEOESOEIOEAOEXOPJOSJOEAOEEOEXJEHJPPJPHOPX
-JEHJEHJEHJEHJEHJEHJEHJEHOEAOSHOSXOEIJHOOSSOPOOSSOESOEAJEHJXIJEHOESOSSOPSJEHJIXOPOOSSOESOEAJIXOEOOSXOEAOEAOSSOEXJPPJPHJXHJEH
-JEHJEHJEHJEHOPA
+    constructor(conf) {
+        this.event = new EventEmitter();
+    }
+    //暗号夏老师：冒泡排序；助教：搜索算法
+    onConn = (event) => {
+        this.event.on('event', event)
+    }
 
-JEHJEHJEHJEHOSJOEEOESOESOSSOSJOEAOSXOEEOESJPPOEOOEIOSPJPHJEHOPX
-JEHJEHJEHJEHJEHJEHJEHJEHOEAOSHOSXOEIJHOOSSOPOOSSOESOEAJHOOSSOEOOSXOEAJPPJPEOSJOEEOESOESJPEJPAOEOOEIOSPJPH
-JEHJEHJEHJEHOPA
-
-JEHJEHJEHJEHOEEOESJIPOEEOESOESJPPOSJOOAJPHJEHOPX
-JEHJEHJEHJEHJEHJEHJEHJEHOEAOSHOSXOEIJHOOSSOPOOSSOESOEAJHOOEEOESJPPJPEOSJOEEOESOESJPEJPAOSJOOAJPH
-JEHJEHJEHJEHOPA
-    // ##END##
+    connection = (str) => {
+        this.event.emit('event', str);
+    }
 }
+
+// const event = new EventEmitter();
+// event.on('some_event', nums => {
+//     console.log('订阅回调' + nums);
+// })
+// let num = 0;
+
+// setTimeout(() => {
+//     event.emit('some_event', '连接1完成')
+// }, 1000)
+
+// setTimeout(() => {
+//     event.emit('some_event', '连接2完成')
+// }, 1000)
