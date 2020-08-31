02080f3d166cc604355d0f957d6f4f7198636d77
岳修斌 <xiubin.yue@guanaitong.com>
2020-08-31 23:29:28 +0800

    06

--- ex06/index.js
+++ ex06/index.js
@@ -4,19 +4,17 @@ module.exports.createToken = token => {
     if (ary.length !== 3) {
         return
     }
-
+    //暗号：贪心算法
     return {
         getExp: () => {
-            // ##BEGIN## 代码已加密
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOSJOEEOESOEIOEAJEHOEPOOIOPPOEJOEEOOIOSOJEHJXIJEHJASOJSJAIJAXJHOOEPOOIOEXOEIOSSJPPOESOSSOPSJEHJIEOPJOSEOSEOSSOEXJHOOSEOEXOEEOEOJPPOOIOEXOPPOOOJHPOOEJPAJEHJPEOOAOOIOEIOSSJXJJHIJPEJPHJPH
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOEAOPJOEXOESJEHOEPOOIOPPOEJOEEOOIOSOJHOOSSOPEOEP
-            // ##END##
+            const data = JSON.parse(Buffer.from(ary[1], 'base64').toString());
+            return data.exp;
         },
 
         verify: key => {
-            const hmac = crypto.createHmac('SHA256', key).update(ary[0]+ '.' +  ary[1]).digest('base64');
+            const hmac = crypto.createHmac('SHA256', key).update(ary[0] + '.' + ary[1]).digest('base64');
             return hmac === ary[2] + '='
-            
+
         }
     }
 }
