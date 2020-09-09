9d3159d6054164781030746e5d92e5b904b1c1c8
岳修斌 <xiubin.yue@guanaitong.com>
2020-09-09 10:08:02 +0800

    09

--- ex09/index.js
+++ ex09/index.js
@@ -1,18 +1,17 @@
 
 
 module.exports.brackets = (target, property) => {
-    const old = target.prototype[property]
+    const old = target.prototype[property];
     target.prototype[property] = msg => {
-        msg = `[${msg}]`
-        return old(msg)
+        msg = `[${msg}]`;
+        return old(msg);
     }
 }
 module.exports.sender = name => (target, property) => {
-    // ##BEGIN## 代码已加密
-JEHJEHJEHJEHOSJOEEOESOEIOEAJEHOEEOEJOSOJEHJXIJEHOEAOOIOEXOSPOSSOEAJHOOEPOEXOEEOEAOEEOEAOPPOEPOSSOOOOEPOEXOEEOEPOSSOEXOEAOPPOOE
-JEHJEHJEHJEHOEAOOIOEXOSPOSSOEAJHOOEPOEXOEEOEAOEEOEAOPPOEPOSSOOOOEPOEXOEEOEPOSSOEXOEAOPPOOEJEHJXIJEHOEOOEIOSPJEHJXIJXAJEHOPX
-JEHJEHJEHJEHJEHJEHJEHJEHOEOOEIOSPJEHJXIJEHOOXJPJOPXOESOOIOEOOSSOPAJEHJXPJEHJPJOPXOEOOEIOSPOPAOOX
-JEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOEAOPJOEXOESJEHOEEOEJOSOJPPOEOOEIOSPJPH
-JEHJEHJEHJEHOPA
-    // ##END##
+    const old = target.prototype[property];
+    //暗号：回溯算法
+    target.prototype[property] = msg => {
+        msg = `${name} : ${msg}`;
+        return old(msg);
+    }
 }
